aeb3994a813441c7b4161d3f301b8bff83e5ec38
sajan basnet <sajanbasnet75@gmail.com>
2017-07-10 13:39:33 +0545

    all commits

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>video.php</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+   <script src="jquery/jquery.js"></script>
+	<script src="jquery/video.js"></script>
+    
+</head>
+<body>
+<div style="width:100%; height:800px; border:solid 1px black" >
+<center><h1>SCROLL UP</h1></center>
+	</div>
+<div style="height:450px;">  
+<center >  
+<div class="scroll-video "> <!-- for images-->
+        
+         <video src="videos/mildiena.mp4"  autobuffer  controls id="newsVideos" class="img-responsive video-box" 
+         style="border:solid 1px grey;box-shadow: 5px 5px 2px #888888;">
+          </video> 
+        <button class="video-button" style="margin-top:5px;">Close</button>
+        
+     </div>
+   </center>    
+ </div>    
+	<div style="width:100%; height:800px; border:solid 1px black" >
+   <center><h1 style="margin-top:50%;">SCROLL DOWN</h1><center>
+
+	</div>
+</body>
+</html>
